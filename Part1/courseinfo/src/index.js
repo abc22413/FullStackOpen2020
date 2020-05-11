54d4cae5deb6084ae8d54374ed53436c012ac9ef
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2020-05-11 13:22:21 +0800

    Did refactor code with arr

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Header shows course name
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Content shows parts
+const Content = (props) => {
+  const parts = props.parts
+  const nums = props.nums
+  return (
+    <>
+      <Part name={props.parts[0]} num={props.nums[0]} />
+      <Part name={props.parts[1]} num={props.nums[1]} />
+      <Part name={props.parts[2]} num={props.nums[2]} />
+    </>
+  )
+}
+
+//part shows body text row
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.name} {props.num}</p>
+    </>
+  )
+}
+
+//Total shows summary
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.totalnum}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const parts = [part1, part2, part3]
+  const nums = [exercises1, exercises2, exercises3]
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={parts} nums={nums}/>
+      <Total totalnum={exercises1+exercises2+exercises3}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
