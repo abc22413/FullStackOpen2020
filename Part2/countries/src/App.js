c0daac7e6f435cd122c8173e86d59899a55e1986
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2020-05-15 17:47:24 +0800

    made base, now implementing functionality

--- Part2/countries/src/App.js
+++ Part2/countries/src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Result = () => {
+
 }
 
-export default App;
+const Entry = () => {
+
+}
+
+const Content = ({ countries, search }) => {
+  //If no search
+  if (!search) {
+    return (
+      <>
+      <p>Type a country's name to learn more</p>
+      </>
+    )
+  }
+  //Too many countries
+  else if(countries.length > 10) {
+    return (
+      <>
+      <p>Your search criteria are too broad.</p>
+      </>
+    )
+  }
+  //No countries found
+  else if(countries.length === 0) {
+    return (
+      <>
+      <p>No country matches your search criteria.</p>
+      </>
+    )
+  }
+  else if(countries.length === 1) {
+    return (
+      <>
+      <p>One Country found</p>
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+      <p>You found {countries.length} countries</p>
+      </>
+    )
+  }
+}
+
+const App = () => {
+  const [search, setSearch] = useState("")
+  const [countries, setCountries] = useState([])
+  const [allCountries, setAllCountries] = useState([])
+
+  useEffect(
+    () => {
+      axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then(response => setAllCountries(response.data))
+    }
+  ,[])
+
+  const handleSearchChange = (event) => {
+    let newSearch = event.target.value
+    setSearch(newSearch)
+    const filteredCountries = allCountries.filter(country => country.name.toLowerCase().includes(newSearch.toLowerCase()))
+    setCountries(filteredCountries)
+  }
+
+  return (
+    <>
+    <div>find countries <input value={search} onChange={handleSearchChange} /></div>
+    <Content countries={countries} search={search}/>
+    </>
+  )
+}
+
+export default App
